c41d27e7ca11d939c434765d5277b00dd09bedb6
matty1203 <mathews.philip@mermerapps.com>
2021-06-03 01:13:55 +0100

    Made the site responsive

--- src/app/animation.ts
+++ src/app/animation.ts
@@ -17,19 +17,19 @@ import {
           left: 0,
           width: '100%'
         })
-      ]),
+      ] ,{ optional: true }),
       query(':enter', [
         style({ left: '-100%' })
-      ]),
-      query(':leave', animateChild()),
+      ],{ optional: true }),
+      query(':leave', animateChild(),{ optional: true }),
       group([
         query(':leave', [
           animate('300ms ease-out', style({ left: '100%' }))
-        ]),
+        ],{ optional: true }),
         query(':enter', [
           animate('300ms ease-out', style({ left: '0%' }))
-        ])
+        ],{ optional: true })
       ]),
-      query(':enter', animateChild()),
+      query(':enter', animateChild(),{ optional: true }),
     ])
   ]);
