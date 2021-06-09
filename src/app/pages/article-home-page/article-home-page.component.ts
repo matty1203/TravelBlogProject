cb18ceda2fb469bba43d1d94fd8d664d553e5889
matty1203 <mathews.philip@mermerapps.com>
2021-06-09 18:36:04 +0100

    Content pages

--- src/app/pages/article-home-page/article-home-page.component.ts
+++ src/app/pages/article-home-page/article-home-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
-import{slideUpAnimation} from './content_animation'
+import { slideUpAnimation } from './content_animation'
 @Component({
   selector: 'app-article-home-page',
   templateUrl: './article-home-page.component.html',
@@ -15,39 +15,48 @@ import{slideUpAnimation} from './content_animation'
 export class ArticleHomePageComponent implements OnInit {
 
 
-  data:any={id:111,name:"EStonia"}
+  data: any = { id: 111, name: "Estonia" }
+  prev_active: number = 0;
   images = [700, 533, 807, 124].map((n) => `https://picsum.photos/id/${n}/900/500`);
-menu:any=[{id:1,name:"Geographic Location",url:"/article-home/geographic-location"},
-{id:1,name:"How to reach",url:"/article-home/how-to-reach"},
-{id:1,name:"Food and Culture",url:"/article-home/food-and-culture"},
-{id:1,name:"Safety",url:"/article-home/safety"},
-{id:1,name:"Travel Stories",url:"/article-home/travel-stories"},
-{id:1,name:"Photo Gallery",url:"/article-home/article-landing"},
-{id:1,name:"Feature Videos",url:"/article-home/article-landing"}]
+  menu: any = [{ id: 1, name: "Geographic Location", url: "/article-home/geographic-location" },
+  { id: 2, name: "How to reach", url: "/article-home/how-to-reach" },
+  { id: 3, name: "Food and Culture", url: "/article-home/food-and-culture" },
+  { id: 4, name: "Safety", url: "/article-home/safety" },
+  { id: 5, name: "Travel Stories", url: "/article-home/travel-stories" },
+  { id: 6, name: "Photo Gallery", url: "/article-home/article-landing" },
+  { id: 7, name: "Feature Videos", url: "/article-home/article-landing" }]
 
 
-  constructor(private router:Router) {
+  constructor(private router: Router) {
 
   }
   ngOnInit(): void {
   }
 
-  
-  scrollTo(){
+
+  scrollTo() {
     var elmnt = document.getElementById("intro3");
     elmnt.scrollIntoView();
   }
- onNavigate(url:any){
-   console.log(url)
-  this.router.navigate([url])
- }
- toLandingPage(){
-   
-  this.router.navigate(['/article-home/article-landing'])
- }
- prepareRoute(outlet: RouterOutlet) {
-  return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
-}
+  onNavigate(url: any, id: any) {
+    if (this.prev_active != 0) {
+      console.log("Hi")
+      var prev_element = document.getElementById('menu_item' + this.prev_active);
+      prev_element.classList.remove("menu--click");
+    }
+  /////To store the previous active element
+    this.prev_active = id;
+    var element = document.getElementById('menu_item' + id);
+    element.classList.add("menu--click");
+    this.router.navigate([url])
+  }
+  toLandingPage() {
+
+    this.router.navigate(['/article-home/article-landing'])
+  }
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
+  }
 
 
 }
